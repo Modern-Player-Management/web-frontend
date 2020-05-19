56f02d120be60612273ba38e7a011463cd87a9c8
Thomas LACAZE <thomas.lacaze2@eiffage.com>
2020-05-19 17:11:17 +0200

    Updates

---
+++ precache-manifest.df194008bdba370885536a815b86f9e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b276fb6725e61f9208af15d0ac64a2",
+    "url": "/web-frontend/index.html"
+  },
+  {
+    "revision": "00c41de90273d2e1d09f",
+    "url": "/web-frontend/static/css/main.2862e00e.chunk.css"
+  },
+  {
+    "revision": "12e980fa796db200ccca",
+    "url": "/web-frontend/static/js/2.247abd2e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/web-frontend/static/js/2.247abd2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c41de90273d2e1d09f",
+    "url": "/web-frontend/static/js/main.2f2388f2.chunk.js"
+  },
+  {
+    "revision": "8c34860ff6ed331d3f17",
+    "url": "/web-frontend/static/js/runtime-main.4705f9dd.js"
+  }
+]);
