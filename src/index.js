fd8bc7e998ab9b2f0f3bad71c0f3ee0900d3a293
Thomas LACAZE <thomas.lacaze2@eiffage.com>
2020-05-19 17:55:55 +0200

    test github page

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={"/web-frontend/"}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
