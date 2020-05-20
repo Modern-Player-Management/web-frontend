19ac851a404ffe33a7645e7f752bc5445bd9e717
Thomas LACAZE <thomas.lacaze2@eiffage.com>
2020-05-20 17:52:37 +0200

    new title name

--- src/components/CardTeams.component.js
+++ src/components/CardTeams.component.js
@@ -1,11 +1,9 @@
-import React from "react";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import red from "@material-ui/core/colors/red";
+import CardHeader from "@material-ui/core/CardHeader";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import Typography from "@material-ui/core/Typography";
+import React from "react";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -16,19 +14,6 @@ const useStyles = makeStyles((theme) => ({
         height: 0,
         paddingTop: '56.25%', // 16:9
     },
-    expand: {
-        transform: 'rotate(0deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
-    avatar: {
-        backgroundColor: red[500],
-    },
 }));
 
 
@@ -37,26 +22,36 @@ export default function CardTeams(props) {
 
     return (
         <Card className={classes.root}>
-            <CardHeader
-                title={props.team.name}
-                subheader={props.team.manager.username}
-            />
-            <CardMedia
+            <CardActionArea>
+                <CardHeader
+                    title={props.team.name}
+                    subheader={props.team.manager.username}
+                />
+                {/*             <CardMedia
                 className={classes.media}
                 image="/static/images/cards/paella.jpg"
                 title={props.team.name}
-            />
-            <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    {
-                        props.team.members.length !== 0 ?  props.team.members.map((member, index) => {
-                            return (
-                                member.username
-                            )
-                        }):"There is no member..."
-                    }
-                </Typography>
-            </CardContent>
+            /> */}
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {
+                            props.team.members.length !== 0 ? props.team.members.map((member, index) => {
+                                return (
+                                    member.username
+                                )
+                            }) : "There is no member..."
+                        }
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Share
+        </Button>
+                <Button size="small" color="primary">
+                    Learn More
+        </Button>
+            </CardActions>
         </Card>
     )
 }
