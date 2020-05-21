d7b1a064d134ec04ffcdfaff7af18d8bfa62f4cd
Thomas LACAZE <contact@thomaslacaze.fr>
2020-05-21 18:33:03 +0200

    remove useless import

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ import MenuIcon from "@material-ui/icons/Menu";
 import clsx from "clsx";
 import IconButton from "@material-ui/core/IconButton";
 import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 import Home from "./components/home.component";
 import Register from "./components/register.component";
 import Login from "./components/login.component";
