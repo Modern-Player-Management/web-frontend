3ae0b2e3a804f87ea94ec9f3ee09afe9f4ad4a3f
Thomas LACAZE <contact@thomaslacaze.fr>
2020-05-24 20:24:41 +0200

    get token from sessionStorage

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,5 +1,5 @@
 export default function authHeader() {
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(sessionStorage.getItem('user'));
 
   if (user && user.token) {
     return { 'Authorization': 'Bearer '+ user.token };       // for Node.js Express back-end
