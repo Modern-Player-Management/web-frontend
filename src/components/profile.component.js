73a69d9e5ae9287f8b9d03cdda919b96b361ec11
Thomas LACAZE <contact@thomaslacaze.fr>
2020-05-31 14:57:22 +0200

    test user profile

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -1,7 +1,44 @@
 import React, { Component } from "react";
 import AuthService from "../services/auth.service";
+import CardHeader from "@material-ui/core/CardHeader";
+import Form from "react-validation/build/form";
+import CardContent from "@material-ui/core/CardContent";
+import TextField from "@material-ui/core/TextField";
+import Alert from "@material-ui/lab/Alert";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import CheckButton from "react-validation/build/button";
+import Card from "@material-ui/core/Card";
+import {withStyles} from "@material-ui/core/styles";
 
-export default class Profile extends Component {
+const useStyles = (theme) => ({
+    root: {
+        width: 343,
+        borderRadius: 12,
+        padding: 12,
+        marginTop: 25,
+        textAlign: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        boxShadow: 'rgba(34, 35, 58, 0.2) 0px 14px 80px',
+        transition: 'all 0.3s ease 0s',
+    },
+    alert: {
+        boxSizing: 'border-box',
+    },
+    actions:{
+        justifyContent: 'center'
+    },
+    textfield:{
+        margin: 10,
+    },
+    button:{
+        width: '45%',
+    }
+});
+
+
+class Profile extends Component {
   constructor(props) {
     super(props);
 
@@ -11,25 +48,87 @@ export default class Profile extends Component {
   }
 
   render() {
-    const { currentUser } = this.state;
+    const { classes } = this.props;
 
     return (
-      <div className="container">
-        <header className="jumbotron">
-          <h3>
-            <strong>{currentUser.username}</strong> Profile
-          </h3>
-        </header>
-        <p>
-          <strong>Id:</strong>{" "}
-          {currentUser.id}
-        </p>
-        <p>
-          <strong>Email:</strong>{" "}
-          {currentUser.email}
-        </p>
-          {currentUser.token}
-      </div>
+        <Card className={classes.root}>
+            <CardHeader
+                title="Profile"
+            />
+            <Form
+                noValidate
+                autoComplete="off"
+                onSubmit={this.handleRegister}
+                ref={c => {
+                    this.form = c;
+                }}
+            >
+                {!this.state.successful && (
+                    <>
+                        <CardContent>
+                            <TextField
+                                id="standard-basic"
+                                type="text"
+                                label="Username"
+                                name="username"
+                                className={classes.textfield}
+                                value={this.state.username}
+                                onChange={this.onChangeUsername}
+                                required={true}
+                            />
+                            <TextField
+                                id="standard-basic"
+                                label="Email"
+                                type="text"
+                                className={classes.textfield}
+                                name="email"
+                                value={this.state.email}
+                                onChange={this.onChangeEmail}
+                                required={true}
+                            />
+                            <TextField
+                                id="standard-basic"
+                                type="password"
+                                label="Password"
+                                name="password"
+                                className={classes.textfield}
+                                value={this.state.password}
+                                onChange={this.onChangePassword}
+                                required={true}
+                            />
+
+                            {this.state.message && (
+                                <Alert
+                                    className={classes.alert}
+                                    severity={
+                                        this.state.successful
+                                            ? "success"
+                                            : "error"
+                                    }
+                                >{this.state.message}</Alert>
+                            )}
+                        </CardContent>
+
+                        <CardActions className={classes.actions}>
+                            <Button
+                                className={classes.button}
+                                type="submit"
+                                variant="contained"
+                                color="primary">Sign Up</Button>
+                        </CardActions>
+                    </>
+                )}
+
+                <CheckButton
+                    style={{display: "none"}}
+                    ref={c => {
+                        this.checkBtn = c;
+                    }}
+                />
+            </Form>
+        </Card>
     );
   }
 }
+
+export default withStyles(useStyles)(Profile);
